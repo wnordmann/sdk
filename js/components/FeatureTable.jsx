91255c9bf3b5139863f85f5519a63cb39f66e9b2
Bart van den Eijnden <bartvde@boundlessgeo.com>
2015-07-01 17:46:28 +0200

    Introduce a FeatureStore, and WIP on resizing columns

--- js/components/FeatureTable.jsx
+++ js/components/FeatureTable.jsx
@@ -2,21 +2,33 @@
 import React from 'react';
 import FixedDataTable from 'fixed-data-table';
 import '../../node_modules/fixed-data-table/dist/fixed-data-table.css';
+import FeatureStore from '../stores/FeatureStore.js';
 
 export default class FeatureTable extends React.Component {
   constructor(props) {
     super(props);
-    var source = this.props.layer.getSource();
+    this._store = new FeatureStore({layer: this.props.layer});
     this.state = {
-      features: []
+      features: [],
+      columnWidths: {}
     };
-    source.on('change', function(evt) {
-      if (evt.target.getState() === 'ready') {
-        this.setState({
-          features: evt.target.getFeatures()
-        });
-      }
-    }, this);
+  }
+  componentWillMount() {
+    this._store.addChangeListener(this._onChange.bind(this));
+    this._onChange();
+  }
+  _onChange() {
+    this.setState(this._store.getState());
+  }
+  _rowGetter(index) {
+    var obj = this.state.features[index].getProperties();
+    return Object.keys(obj).map(key => obj[key]);
+  }
+  _onColumnResize(width, idx) {
+    var label = this.state.features[0].getKeys()[idx];
+    var columnWidths = this.state.columnWidths;
+    columnWidths[label] = width;
+    this.setState({columnWidths: columnWidths});
   }
   render() {
     var Table = FixedDataTable.Table;
@@ -24,18 +36,32 @@ export default class FeatureTable extends React.Component {
     if (this.state.features.length > 0) {
       var me = this, idx = -1;
       var feature = this.state.features[0], geom = feature.getGeometryName();
-      var columnNodes = feature.getKeys().map(function(key) {
+      var columnNodes = feature.getKeys().map(function(key, idx) {
         if (key !== geom) {
-          idx++;
-          return <Column label={key} dataKey={idx} key={key} width={100} />;
+          if (!me.state.columnWidths[key]) {
+            me.state.columnWidths[key] = 100;
+          }
+          return (
+            <Column
+              isResizable={true}
+              label={key}
+              dataKey={idx}
+              key={key}
+              width={me.state.columnWidths[key]} />
+          );
         }
       });
-      var rowGetter = function(rowIndex) {
-        var obj = me.state.features[rowIndex].getProperties();
-        delete obj[geom];
-        return Object.keys(obj).map(key => obj[key]);
-      };
-      return <Table rowHeight={50} rowGetter={rowGetter} headerHeight={50} rowsCount={this.state.features.length} width={400} height={400}>{columnNodes}</Table>;
+      return (
+        <Table
+          onColumnResizeEndCallback={this._onColumnResize.bind(this)}
+          rowHeight={30}
+          rowGetter={this._rowGetter.bind(this)}
+          headerHeight={50}
+          rowsCount={this.state.features.length}
+          width={400}
+          height={400}>
+          {columnNodes}
+        </Table>);
     } else {
       return false;
     }
---
+++ js/stores/FeatureStore.js
@@ -0,0 +1,37 @@
+/* global document */
+
+import {EventEmitter} from 'events';
+
+export default class FeatureStore extends EventEmitter {
+  constructor(props) {
+    super();
+    if (props.layer) {
+      this.bindLayer(props.layer);
+    }
+  }
+  bindLayer(layer) {
+    this._config = {};
+    this._layer = layer;
+    var source = layer.getSource();
+    source.on('change', function(evt) {
+      if (evt.target.getState() === 'ready') {
+        this._config.features = evt.target.getFeatures();
+        this.emitChange();
+      }
+    }, this);
+    this._config.features = source.getFeatures();
+    this.emitChange();
+  }
+  getState() {
+    return this._config;
+  }
+  emitChange() {
+    this.emit('CHANGE');
+  }
+  addChangeListener(cb) {
+    this.on('CHANGE', cb);
+  }
+  removeChangeListener(cb) {
+    this.removeListener('CHANGE', cb);
+  }
+}
