7711fed33b4fcae43d7e3298d87b1efc1d5922a5
Bart van den Eijnden <bartvde@boundlessgeo.com>
2015-09-22 17:16:23 +0200

    Add docs for chart

--- js/components/Chart.jsx
+++ js/components/Chart.jsx
@@ -13,6 +13,9 @@ const DISPLAY_MODE_FEATURE = 0;
 const DISPLAY_MODE_CATEGORY = 1;
 const DISPLAY_MODE_COUNT = 2;
 
+/**
+ * Allow for the creation of charts based on selected features of a layer.
+ */
 export default class Chart extends React.Component {
   constructor(props) {
     super(props);
@@ -184,8 +187,28 @@ export default class Chart extends React.Component {
 }
 
 Chart.propTypes = {
+  /**
+   * An array of configuration objects. Configuration objects have title, categoryField, layer,
+   * valueFields, displayMode and operation keys.
+   * title is the title to display for the chart.
+   * categoryField is the attribute to use as the category.
+   * layer is the id property of the corresponding layer to use.
+   * valueFields is an array of field names to use for displaying values in the chart.
+   * displayMode defines how the feature attributes will be used to create the chart. When using a value of 0 (by feature), an element will
+   * be added to the chart for each selected feature. When using a value of 1 (by category), selected features will be grouped according to
+   * a category defined by the categoryField. When using a value of 2 (count by category) the chart will show the number of features in each
+   * category.
+   * The statistic function to use when displayMode is by category (1) is defined in the operation key.
+   * A value of 0 means MIN, a value of 1 means MAX, a value of 2 means SUM and a value of 3 means AVG (Average).
+   */
   charts: React.PropTypes.array.isRequired,
+  /**
+   * If true, show a combo box to select charts instead of dropdown button.
+   */
   combo: React.PropTypes.bool,
+  /**
+   * The id of the container to show when a chart is selected.
+   */
   container: React.PropTypes.string
 };
 
