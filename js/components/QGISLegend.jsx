d1d0e492a74efeb253b372b2cbe50cc6d5359b6e
Bart van den Eijnden <bartvde@boundlessgeo.com>
2015-10-12 10:30:04 +0200

    Add missing require

--- js/components/QGISLegend.jsx
+++ js/components/QGISLegend.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ol from 'openlayers';
 import UI from 'pui-react-buttons';
 import Icon from 'pui-react-iconography';
 import LayerStore from '../stores/LayerStore.js';
