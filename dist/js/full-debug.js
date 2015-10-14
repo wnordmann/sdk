d9288df3fd7e8a3d3cb1c79c38878dc68a5832a5
Bart van den Eijnden <bartvde@boundlessgeo.com>
2015-10-14 13:14:36 +0200

    Easier way to pass on props to LayerListItem

--- dist/js/full-debug.js
+++ dist/js/full-debug.js
@@ -2928,6 +2928,8 @@ Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
@@ -3023,9 +3025,9 @@ var LayerList = (function (_React$Component) {
       if (lyr.get('hideFromLayerList') !== true) {
         if (lyr instanceof _openlayers2['default'].layer.Group) {
           var children = this.props.showGroupContent ? this.renderLayerGroup(lyr) : undefined;
-          return _react2['default'].createElement(_LayerListItemJsx2['default'], { map: this.props.map, showOpacity: this.props.showOpacity, showDownload: this.props.showDownload, allowReordering: this.props.allowReordering, showZoomTo: this.props.showZoomTo, key: lyr.get('title'), layer: lyr, children: children, title: lyr.get('title') });
+          return _react2['default'].createElement(_LayerListItemJsx2['default'], _extends({}, this.props, { key: lyr.get('title'), layer: lyr, children: children, title: lyr.get('title') }));
         } else {
-          return _react2['default'].createElement(_LayerListItemJsx2['default'], { map: this.props.map, showOpacity: this.props.showOpacity, showDownload: this.props.showDownload, allowReordering: this.props.allowReordering, showZoomTo: this.props.showZoomTo, key: lyr.get('title'), layer: lyr, title: lyr.get('title') });
+          return _react2['default'].createElement(_LayerListItemJsx2['default'], _extends({}, this.props, { key: lyr.get('title'), layer: lyr, title: lyr.get('title') }));
         }
       }
     }
