d162bd034e9a23227346dd1e7cdc0b4d5248fd34
Bart van den Eijnden <bartvde@boundlessgeo.com>
2015-10-26 14:00:32 +0100

    Clean up generate api doc script

--- scripts/generate-api-doc.js
+++ scripts/generate-api-doc.js
@@ -3,26 +3,28 @@ var fs = require('fs');
 var path = require('path')
 
 var dir = 'js/components/';
+var output = 'api/info.json';
 var info = {};
 
-fs.readdir(dir,function(err,files){
-    if (err) throw err;
-    var c=0;
-    files.forEach(function(file){
-        if (path.extname(file) === '.js' || path.extname(file) === '.jsx') {
-        c++;
-        fs.readFile(dir+file,'utf-8',function(err,src){
-            if (err) throw err;
-            var componentInfo = reactDocs.parse(src, reactDocs.resolver.findAllComponentDefinitions);
-            info[dir+file] = componentInfo[0];
-if (0===--c) {
-    var result = JSON.stringify(info);
-    fs.writeFileSync('api/info.json', result);
-            }
-        });
+fs.readdir(dir, function(err, files) {
+  if (err) {
+    throw err;
+  }
+  var c = 0;
+  files.forEach(function(file) {
+    if (path.extname(file) === '.js' || path.extname(file) === '.jsx') {
+      c++;
+      fs.readFile(dir + file, 'utf-8', function(err,src) {
+        if (err) {
+          throw err;
         }
-    });
+        var componentInfo = reactDocs.parse(src, reactDocs.resolver.findAllComponentDefinitions);
+        info[dir+file] = componentInfo[0];
+        if (0 === --c) {
+          var result = JSON.stringify(info);
+          fs.writeFileSync(output, result);
+        }
+      });
+    }
+  });
 });
-
-//var componentInfo = reactDocs.parse(fs.readFileSync('js/components/QGISPrint.jsx'), reactDocs.resolver.findAllComponentDefinitions);
-//console.log(componentInfo);
