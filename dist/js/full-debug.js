f7bdfbfb6ab180832057e4916191b1526b15cb16
Bart van den Eijnden <bartvde@boundlessgeo.com>
2015-11-25 15:35:45 +0100

    React 0.14.3

--- dist/js/full-debug.js
+++ dist/js/full-debug.js
@@ -76198,6 +76198,7 @@ var HTMLDOMPropertyConfig = {
     multiple: MUST_USE_PROPERTY | HAS_BOOLEAN_VALUE,
     muted: MUST_USE_PROPERTY | HAS_BOOLEAN_VALUE,
     name: null,
+    nonce: MUST_USE_ATTRIBUTE,
     noValidate: HAS_BOOLEAN_VALUE,
     open: HAS_BOOLEAN_VALUE,
     optimum: null,
@@ -76209,6 +76210,7 @@ var HTMLDOMPropertyConfig = {
     readOnly: MUST_USE_PROPERTY | HAS_BOOLEAN_VALUE,
     rel: null,
     required: HAS_BOOLEAN_VALUE,
+    reversed: HAS_BOOLEAN_VALUE,
     role: MUST_USE_ATTRIBUTE,
     rows: MUST_USE_ATTRIBUTE | HAS_POSITIVE_NUMERIC_VALUE,
     rowSpan: null,
@@ -76650,6 +76652,7 @@ assign(React, {
 });
 
 React.__SECRET_DOM_DO_NOT_USE_OR_YOU_WILL_BE_FIRED = ReactDOM;
+React.__SECRET_DOM_SERVER_DO_NOT_USE_OR_YOU_WILL_BE_FIRED = ReactDOMServer;
 
 module.exports = React;
 },{"./Object.assign":677,"./ReactDOM":690,"./ReactDOMServer":700,"./ReactIsomorphic":718,"./deprecated":762}],680:[function(require,module,exports){
@@ -86906,7 +86909,7 @@ module.exports = ReactUpdates;
 
 'use strict';
 
-module.exports = '0.14.2';
+module.exports = '0.14.3';
 },{}],741:[function(require,module,exports){
 /**
  * Copyright 2013-2015, Facebook, Inc.
