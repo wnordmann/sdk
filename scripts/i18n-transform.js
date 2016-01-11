e3c0a0096154a439f856ee1fbc624f1e0cbf4ba0
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-01-11 13:58:49 +0100

    Make i18n browserify transform more strict

--- scripts/i18n-transform.js
+++ scripts/i18n-transform.js
@@ -4,7 +4,7 @@ module.exports = function (file) {
   var data = '';
   function write(buf) { data += buf; }
   function end() {
-    this.queue(data.replace(/injectIntl\((.*?)\)/g, '$1'));
+    this.queue(data.replace(/export default injectIntl\((.*?)\)/g, 'export default $1'));
     this.queue(null);
   }
   return through(write, end);
