f115284a2565c8dbc76974df4e9b4d3baf164406
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-03-29 13:09:07 +0200

    Do not pass to JSONIX if ServiceException

--- js/components/AddLayerModal.jsx
+++ js/components/AddLayerModal.jsx
@@ -143,34 +143,36 @@ class AddLayerModal extends Dialog.Modal {
     var url = this._getServiceUrl(me.props.url);
     url = url.replace('wms', 'wfs') + 'service=WFS&request=DescribeFeatureType&version=1.0.0&typename=' + layer.Name;
     doGET(url, function(xmlhttp) {
-      var context = new Jsonix.Context([XSD_1_0]);
-      var unmarshaller = context.createUnmarshaller();
-      var schema = unmarshaller.unmarshalString(xmlhttp.responseText).value;
-      var element = schema.complexType[0].complexContent.extension.sequence.element;
-      var geometryType, geometryName;
-      var attributes = [];
-      for (var i = 0, ii = element.length; i < ii; ++i) {
-        var el = element[i];
-        if (el.type.namespaceURI === 'http://www.opengis.net/gml') {
-          geometryName = el.name;
-          var lp = el.type.localPart;
-          geometryType = lp.replace('PropertyType', '');
-        } else {
-          // TODO if needed, use attribute type as well
-          attributes.push(el.name);
+      if (xmlhttp.responseText.indexOf('ServiceExceptionReport') === -1) {
+        var context = new Jsonix.Context([XSD_1_0]);
+        var unmarshaller = context.createUnmarshaller();
+        var schema = unmarshaller.unmarshalString(xmlhttp.responseText).value;
+        var element = schema.complexType[0].complexContent.extension.sequence.element;
+        var geometryType, geometryName;
+        var attributes = [];
+        for (var i = 0, ii = element.length; i < ii; ++i) {
+          var el = element[i];
+          if (el.type.namespaceURI === 'http://www.opengis.net/gml') {
+            geometryName = el.name;
+            var lp = el.type.localPart;
+            geometryType = lp.replace('PropertyType', '');
+          } else {
+            // TODO if needed, use attribute type as well
+            attributes.push(el.name);
+          }
         }
+        attributes.sort(function(a, b) {
+          return a.toLowerCase().localeCompare(b.toLowerCase());
+        });
+        this.set('wfsInfo', {
+          featureNS: schema.targetNamespace,
+          featureType: schema.element[0].name,
+          geometryType: geometryType,
+          geometryName: geometryName,
+          attributes: attributes,
+          url: me.props.url.replace('wms', 'wfs')
+        });
       }
-      attributes.sort(function(a, b) {
-        return a.toLowerCase().localeCompare(b.toLowerCase());
-      });
-      this.set('wfsInfo', {
-        featureNS: schema.targetNamespace,
-        featureType: schema.element[0].name,
-        geometryType: geometryType,
-        geometryName: geometryName,
-        attributes: attributes,
-        url: me.props.url.replace('wms', 'wfs')
-      });
     }, function(xmlhttp) { }, olLayer);
     // TODO handle failure
   }
