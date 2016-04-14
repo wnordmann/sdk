a211f99e5e2d989e4e3267699c1d9883ba15132e
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-04-14 16:13:13 +0200

    Add test for MapConfig

---
+++ __tests__/components/MapConfig.test.js
@@ -0,0 +1,55 @@
+/* global afterEach, beforeEach, describe, it */
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var assert = require('chai').assert;
+var raf = require('raf');
+raf.polyfill();
+var ol = require('openlayers');
+var intl = require('../mock-i18n.js');
+
+var MapConfig = require('../../js/components/MapConfig.jsx');
+
+describe('MapConfig', function() {
+  var target, map;
+  var width = 360;
+  var height = 180;
+
+  beforeEach(function(done) {
+    target = document.createElement('div');
+    var style = target.style;
+    style.position = 'absolute';
+    style.left = '-1000px';
+    style.top = '-1000px';
+    style.width = width + 'px';
+    style.height = height + 'px';
+    document.body.appendChild(target);
+    map = new ol.Map({
+      target: target,
+      view: new ol.View({
+        center: [0, 0],
+        zoom: 1
+      })
+    });
+    map.once('postrender', function() {
+      done();
+    });
+  });
+
+  afterEach(function() {
+    map.setTarget(null);
+    document.body.removeChild(target);
+  });
+
+
+  it('creates two dropdown menus', function() {
+    var container = document.createElement('div');
+    ReactDOM.render((
+      <MapConfig intl={intl} map={map}/>
+    ), container);
+    var menus = container.querySelectorAll('[role=menuitem]');
+    assert.equal(menus.length, 2);
+    ReactDOM.unmountComponentAtNode(container);
+  });
+
+});
