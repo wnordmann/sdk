207b181b9bd9f1de3d74bfe38a5ad4bdf2e118eb
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-04-25 13:02:55 +0200

    Use Table component in InfoPopup

--- js/components/HomeButton.jsx
+++ js/components/HomeButton.jsx
@@ -12,24 +12,16 @@
 
 import React from 'react';
 import ol from 'openlayers';
-import {defineMessages, injectIntl, intlShape} from 'react-intl';
+import {injectIntl, intlShape} from 'react-intl';
 import IconButton from 'material-ui/lib/icon-button';
 import HomeIcon from 'material-ui/lib/svg-icons/action/home';
 import pureRender from 'pure-render-decorator';
-
-const messages = defineMessages({
-  buttontitle: {
-    id: 'homebutton.buttontitle',
-    description: 'Title for the home button',
-    defaultMessage: 'Home'
-  }
-});
 
 /**
  * A button to go back to the initial extent of the map.
  *
  * ```html
- * <div id='home-button' className='ol-unselectable ol-control'>
+ * <div id='home-button'>
  *   <HomeButton map={map} />
  * </div>
  * ```
@@ -60,7 +52,6 @@ class HomeButton extends React.Component {
     }
   }
   render() {
-    const {formatMessage} = this.props.intl;
     return (
       <IconButton onTouchTap={this._goHome.bind(this)}><HomeIcon /></IconButton>
     );
