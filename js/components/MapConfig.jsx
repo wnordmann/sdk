b915654000899fa26640064d5234f7d73882edd6
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-05-17 13:39:49 +0200

    No need to set value of Map menu (SDK-207)

--- js/components/MapConfig.jsx
+++ js/components/MapConfig.jsx
@@ -50,7 +50,6 @@ const localStorageKey = 'web-sdk-map-config';
 class MapConfig extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: null};
   }
   _load() {
     var config = global.localStorage.getItem(localStorageKey);
@@ -68,12 +67,11 @@ class MapConfig extends React.Component {
     } else if (value === 2) {
       this._save();
     }
-    this.setState({value: value});
   }
   render() {
     const {formatMessage} = this.props.intl;
     return (
-      <IconMenu {...this.props} iconButtonElement={<RaisedButton label={formatMessage(messages.menubuttontext)} />} value={this.state.value} onChange={this._handleChange.bind(this)}>
+      <IconMenu {...this.props} iconButtonElement={<RaisedButton label={formatMessage(messages.menubuttontext)} />} onChange={this._handleChange.bind(this)}>
         <MenuItem value={1} primaryText={formatMessage(messages.loadtext)}/>
         <MenuItem value={2} primaryText={formatMessage(messages.savetext)}/>
       </IconMenu>
