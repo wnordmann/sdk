14b8b9ba9c1856f3cc9d3ca10465e39219b891f9
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-06-10 14:36:13 +0200

    Add back LoginModal unit test

---
+++ __tests__/components/LoginModal.test.js
@@ -0,0 +1,26 @@
+/* global describe, it */
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var assert = require('chai').assert;
+var intl = require('../mock-i18n.js');
+
+var LoginModal = require('../../js/components/LoginModal.jsx');
+
+describe('LoginModal', function() {
+
+  it('inputs for user and password generated', function(done) {
+    var container = document.createElement('div');
+    var login = ReactDOM.render((
+      <LoginModal intl={intl} />
+    ), container);
+    login.open();
+    var inputs = document.querySelectorAll('input');
+    assert.equal(inputs.length, 2);
+    window.setTimeout(function() {
+      ReactDOM.unmountComponentAtNode(container);
+      done();
+    }, 500);
+  });
+
+});
