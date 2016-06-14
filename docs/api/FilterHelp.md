52127ba2c0c36ae688d7e17ba760d5c61f01b957
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-06-14 12:07:52 +0200

    i18n for FilterHelp

---
+++ docs/api/FilterHelp.md
@@ -0,0 +1,46 @@
+`FilterHelp` (component)
+========================
+
+Help dialog for filter fields.
+
+Properties
+----------
+
+### `intl`
+
+i18n message strings. Provided through the application through context.
+
+type: `custom`
+
+
+### `monoStyle`
+
+Style config for all divs that are part of a list item.
+
+type: `object`
+defaultValue: `{
+  fontWeight: 'bold',
+  color: '#424242',
+  display: 'inline-block',
+  minWidth: '80px'
+}`
+
+
+### `popStyle`
+
+Style config for the PopOver.
+
+type: `object`
+defaultValue: `{
+  padding: '12px',
+  margin: '36px',
+  maxWidth: '600px'
+}`
+
+
+### `style`
+
+Style for the root span.
+
+type: `object`
+
--- js/components/FilterHelp.jsx
+++ js/components/FilterHelp.jsx
@@ -14,8 +14,170 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Popover from 'material-ui/lib/popover/popover';
 import HelpOutline from 'material-ui/lib/svg-icons/action/help-outline';
+import {defineMessages, injectIntl, intlShape} from 'react-intl';
 import './FilterHelp.css';
 
+const messages = defineMessages({
+  introtext: {
+    id: 'filterhelp.introtext',
+    description: 'Intro text for the filter help dialog',
+    defaultMessage: 'Type in a string to search all feature attributes, or a filter expression to narrow your search to one or more attributes.'
+  },
+  exampletext: {
+    id: 'filterhelp.exampletext',
+    description: 'Example text',
+    defaultMessage: 'Examples:'
+  },
+  example1filter: {
+    id: 'filterhelp.example1filter',
+    description: 'First example filter text',
+    defaultMessage: 'population > 100000'
+  },
+  example1description: {
+    id: 'filterhelp.example1description',
+    description: 'First example filter description',
+    defaultMessage: 'All features with a population greater than 100000'
+  },
+  example2filter: {
+    id: 'filterhelp.example2filter',
+    description: 'Second example filter text',
+    defaultMessage: 'sovereignt == "United Kingdom" and not (subregion like "Europe")'
+  },
+  example2description: {
+    id: 'filterhelp.example2description',
+    description: 'Second example filter description',
+    defaultMessage: 'All british colonies not in Europe'
+  },
+  generalsection: {
+    id: 'filterhelp.generalsection',
+    description: 'General section text',
+    defaultMessage: 'Expressions may contain attribute names, strings, numbers, and operators. Attribute names may be surrounded  by single quotes (\') to reduce ambiguity. Strings must be surrounded by double quotes ("). Numbers and operators are unquoted.'
+  },
+  operatortext: {
+    id: 'filterhelp.operatortext',
+    description: 'Help text for operators',
+    defaultMessage: 'Supported operators are:'
+  },
+  operator1: {
+    id: 'filterhelp.operator1',
+    description: 'First example of an operator',
+    defaultMessage: 'a == a'
+  },
+  operator1description: {
+    id: 'filterhelp.operator1description',
+    description: 'Desscription for first operator',
+    defaultMessage: 'a exactly equals b'
+  },
+  operator2: {
+    id: 'filterhelp.operator2',
+    description: 'Second example of an operator',
+    defaultMessage: 'a != a'
+  },
+  operator2description: {
+    id: 'filterhelp.operator2description',
+    description: 'Desscription for second operator',
+    defaultMessage: 'a does not equal b'
+  },
+  operator3: {
+    id: 'filterhelp.operator3',
+    description: 'Third example of an operator',
+    defaultMessage: 'a < a'
+  },
+  operator3description: {
+    id: 'filterhelp.operator3description',
+    description: 'Desscription for third operator',
+    defaultMessage: 'a is less than b'
+  },
+  operator4: {
+    id: 'filterhelp.operator4',
+    description: 'Fourth example of an operator',
+    defaultMessage: 'a <= b'
+  },
+  operator4description: {
+    id: 'filterhelp.operator4description',
+    description: 'Desscription for fourth operator',
+    defaultMessage: 'a is less than or equal to b'
+  },
+  operator5: {
+    id: 'filterhelp.operator5',
+    description: 'Fifth example of an operator',
+    defaultMessage: 'a > b'
+  },
+  operator5description: {
+    id: 'filterhelp.operator5description',
+    description: 'Desscription for fifth operator',
+    defaultMessage: 'a is greater than b'
+  },
+  operator6: {
+    id: 'filterhelp.operator6',
+    description: 'Sixth example of an operator',
+    defaultMessage: 'a >= b'
+  },
+  operator6description: {
+    id: 'filterhelp.operator6description',
+    description: 'Desscription for sixth operator',
+    defaultMessage: 'a is greater than or equal to b'
+  },
+  operator7: {
+    id: 'filterhelp.operator7',
+    description: 'Seventh example of an operator',
+    defaultMessage: 'a like b'
+  },
+  operator7description: {
+    id: 'filterhelp.operator7description',
+    description: 'Desscription for seventh operator',
+    defaultMessage: 'b contains a (case insensitive)'
+  },
+  operator8: {
+    id: 'filterhelp.operator8',
+    description: 'Eigth example of an operator',
+    defaultMessage: 'a in (x,y,z)'
+  },
+  operator8description: {
+    id: 'filterhelp.operator8description',
+    description: 'Desscription for eigth operator',
+    defaultMessage: 'a is equal to one or more values in the list x,y,z'
+  },
+  operator9: {
+    id: 'filterhelp.operator9',
+    description: 'Nineth example of an operator',
+    defaultMessage: 'e and f'
+  },
+  operator9description: {
+    id: 'filterhelp.operator9description',
+    description: 'Desscription for nineth operator',
+    defaultMessage: 'Matches both expressions e and f'
+  },
+  operator10: {
+    id: 'filterhelp.operator10',
+    description: 'Tenth example of an operator',
+    defaultMessage: 'e or f'
+  },
+  operator10description: {
+    id: 'filterhelp.operator10description',
+    description: 'Desscription for tenth operator',
+    defaultMessage: 'Matches either expression e or f'
+  },
+  operator11: {
+    id: 'filterhelp.operator11',
+    description: 'Eleventh example of an operator',
+    defaultMessage: 'not e'
+  },
+  operator11description: {
+    id: 'filterhelp.operator11description',
+    description: 'Desscription for eleventh operator',
+    defaultMessage: 'Returns all results that do not match the expression e'
+  },
+  parantheses: {
+    id: 'filterhelp.parantheses',
+    description: 'Help text for parentheses',
+    defaultMessage: 'Any expression may be surrounded by parentheses for clarity (e.g. when combining ands and ors).'
+  }
+});
+
+/**
+ * Help dialog for filter fields.
+ */
 class FilterHelp extends React.Component {
   constructor(props) {
     super(props);
@@ -25,49 +187,47 @@ class FilterHelp extends React.Component {
     this.helpElement = ReactDOM.findDOMNode(this.refs.help);
   }
   _onToggleHelp() {
-    this.setState({help:!this.state.help});
+    this.setState({help: !this.state.help});
   }
   render() {
-    const monoStyle = {'fontWeight':'bold', 'color':'#424242', 'display':'inline-block', 'minWidth':'80px'};
-    const popStyle = {'padding':'12px','margin':'36px','maxWidth':'600px'};
+    const {formatMessage} = this.props.intl;
+    const monoStyle = this.props.monoStyle;
+    const popStyle = this.props.popStyle;
     return (
       <span className='filter-help' style={this.props.style}>
         <HelpOutline ref='help' onClick={this._onToggleHelp.bind(this)}/>
         <Popover open={this.state.help} onRequestClose={this._onToggleHelp.bind(this)} style={popStyle} anchorEl={this.helpElement} anchorOrigin={{'horizontal':'left'}}>
           <p>
-            Type in a string to search all feature attributes, or a filter expression to narrow your search to one or more attributes.
+            {formatMessage(messages.introtext)}
           </p>
           <p>
-            Examples:
-          </p>
-          <ul>
-            <li><div style={monoStyle}>population &gt; 100000 </div><br/>All features with a population greater than 100000</li>
-            <li><div style={monoStyle}>sovereignt == "United Kingdom" and not (subregion like "Europe")</div><br/>All british colonies not in Europe</li>
-          </ul>
-          <p>
-            Expressions may contain attribute names, strings, numbers, and operators.
-            Attribute names may be surrounded  by single quotes (') to reduce ambiguity.
-            Strings must be surrounded by double quotes (").
-            Numbers and operators are unquoted.
-          </p>
-          <p>
-            Supported operators are:
+            {formatMessage(messages.exampletext)}
           </p>
           <ul>
-            <li><div style={monoStyle}>a == a</div> a exactly equals b</li>
-            <li><div style={monoStyle}>a != a</div> a does not equal b</li>
-            <li><div style={monoStyle}>a &lt; a</div> a is less than b</li>
-            <li><div style={monoStyle}>a &lt;= b</div> a is less than or equal to b</li>
-            <li><div style={monoStyle}>a &gt; b</div> a is greater than b</li>
-            <li><div style={monoStyle}>a &gt;= b</div> a is greater than or equal to b</li>
-            <li><div style={monoStyle}>a like b</div> b contains a (case insensitive)</li>
-            <li><div style={monoStyle}>a in (x,y,z)</div> a is equal to one or more values in the list x,y,z</li>
-            <li><div style={monoStyle}>e and f</div> Matches both expressions e and f</li>
-            <li><div style={monoStyle}>e or f</div> Matches either expression e or f</li>
-            <li><div style={monoStyle}>not e</div> Returns all results that do not match the expression e</li>
+            <li><div style={monoStyle}>{formatMessage(messages.example1filter)}</div><br/>{formatMessage(messages.example1description)}</li>
+            <li><div style={monoStyle}>{formatMessage(messages.example2filter)}</div><br/>{formatMessage(messages.example2description)}</li>
           </ul>
           <p>
-            Any expression may be surrounded by parentheses for clarity (e.g. when combining ands and ors).
+            {formatMessage(messages.generalsection)}
+          </p>
+          <p>
+            {formatMessage(messages.operatortext)}
+          </p>
+          <ul>
+            <li><div style={monoStyle}>{formatMessage(messages.operator1)}</div>{formatMessage(messages.operator1description)}</li>
+            <li><div style={monoStyle}>{formatMessage(messages.operator2)}</div>{formatMessage(messages.operator2description)}</li>
+            <li><div style={monoStyle}>{formatMessage(messages.operator3)}</div>{formatMessage(messages.operator3description)}</li>
+            <li><div style={monoStyle}>{formatMessage(messages.operator4)}</div>{formatMessage(messages.operator4description)}</li>
+            <li><div style={monoStyle}>{formatMessage(messages.operator5)}</div>{formatMessage(messages.operator5description)}</li>
+            <li><div style={monoStyle}>{formatMessage(messages.operator6)}</div>{formatMessage(messages.operator6description)}</li>
+            <li><div style={monoStyle}>{formatMessage(messages.operator7)}</div>{formatMessage(messages.operator7description)}</li>
+            <li><div style={monoStyle}>{formatMessage(messages.operator8)}</div>{formatMessage(messages.operator8description)}</li>
+            <li><div style={monoStyle}>{formatMessage(messages.operator9)}</div>{formatMessage(messages.operator9description)}</li>
+            <li><div style={monoStyle}>{formatMessage(messages.operator10)}</div>{formatMessage(messages.operator10description)}</li>
+            <li><div style={monoStyle}>{formatMessage(messages.operator11)}</div>{formatMessage(messages.operator11description)}</li>
+          </ul>
+          <p>
+            {formatMessage(messages.parantheses)}
           </p>
         </Popover>
       </span>
@@ -77,9 +237,35 @@ class FilterHelp extends React.Component {
 
 FilterHelp.propTypes = {
   /**
+   * Style config for all divs that are part of a list item.
+   */
+  monoStyle: React.PropTypes.object,
+  /**
+   * Style config for the PopOver.
+   */
+  popStyle: React.PropTypes.object,
+  /**
    * Style for the root span.
    */
-  style: React.PropTypes.object
+  style: React.PropTypes.object,
+  /**
+   * i18n message strings. Provided through the application through context.
+   */
+  intl: intlShape.isRequired
 };
 
-export default FilterHelp;
+FilterHelp.defaultProps = {
+  monoStyle: {
+    fontWeight: 'bold',
+    color: '#424242',
+    display: 'inline-block',
+    minWidth: '80px'
+  },
+  popStyle: {
+    padding: '12px',
+    margin: '36px',
+    maxWidth: '600px'
+  }
+};
+
+export default injectIntl(FilterHelp);
