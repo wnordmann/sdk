3ebdd75a4b0cb63c127df870a518ef52353111ff
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-11-07 19:03:55 +0100

    More tests for util.js

--- __tests__/root/util.test.js
+++ __tests__/root/util.test.js
@@ -30,4 +30,36 @@ describe('util', function() {
     assert.equal(dim, undefined);
   });
 
+  it('transforms rgb to hex', function() {
+    var rgb = 'rgba(255, 0, 0, 0.5)';
+    assert.equal(Util.rgbToHex(rgb), '#ff0000');
+    rgb = 'rgba(0, 255, 0)';
+    assert.equal(Util.rgbToHex(rgb), '#00ff00');
+    rgb = 'rgb(0, 0, 255)';
+    assert.equal(Util.rgbToHex(rgb), '#0000ff');
+  });
+
+  it('transforms hex to rgb', function() {
+    var hex = '#FF0000';
+    var result = Util.hexToRgb(hex);
+    assert.equal(result.r, 255);
+    assert.equal(result.g, 0);
+    assert.equal(result.b, 0);
+    hex = 'foo';
+    result = Util.hexToRgb(hex);
+    assert.equal(result, null);
+  });
+
+  it('performs JSONP and substitutes __cbname__', function() {
+    Util.doJSONP('/foo?callback=__cbname__');
+    var url;
+    for (var i = 0, ii = document.head.childNodes.length; i < ii; i++) {
+      if (document.head.childNodes[i].tagName === 'SCRIPT') {
+        url = document.head.childNodes[i].src;
+        break;
+      }
+    }
+    assert.equal(url.indexOf('__cbname__'), -1);
+  });
+
 });
