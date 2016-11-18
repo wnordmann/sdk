cb0d4ffe2514bf7f2c6275130e921ae482f05f64
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-11-18 19:46:04 +0100

    Add test case for reorder in layer group

--- __tests__/stores/LayerStore.test.js
+++ __tests__/stores/LayerStore.test.js
@@ -10,7 +10,7 @@ var LayerActions = require('../../js/actions/LayerActions.js');
 
 describe('LayerStore', function() {
 
-  var target, map;
+  var target, map, level1, level2, level3, level3a;
   var width = 360;
   var height = 180;
 
@@ -23,20 +23,26 @@ describe('LayerStore', function() {
     style.width = width + 'px';
     style.height = height + 'px';
     document.body.appendChild(target);
+
+    level3 = new ol.layer.Layer({id: 'level3'});
+    level3a = new ol.layer.Layer({id: 'level3a'});
+    level2 = new ol.layer.Group({
+      id: 'level2',
+      layers: [
+        level3,
+        level3a
+      ]
+    });
+    level1 = new ol.layer.Group({
+      id: 'level1',
+      layers: [
+        level2
+      ]
+    });
     map = new ol.Map({
       target: target,
       layers: [
-        new ol.layer.Group({
-          id: 'level1',
-          layers: [
-            new ol.layer.Group({
-              id: 'level2',
-              layers: [
-                new ol.layer.Layer({id: 'level3'})
-              ]
-            })
-          ]
-        })
+        level1
       ],
       view: new ol.View({
         projection: 'EPSG:4326',
@@ -68,13 +74,13 @@ describe('LayerStore', function() {
       count++;
     };
     LayerStore.addChangeListener(onChangeCb);
-    assert.equal(LayerStore.getState().flatLayers.length, 1);
-    map.addLayer(new ol.layer.Vector());
     assert.equal(LayerStore.getState().flatLayers.length, 2);
+    map.addLayer(new ol.layer.Vector());
+    assert.equal(LayerStore.getState().flatLayers.length, 3);
     assert.equal(count, 1);
     map.getLayers().pop();
     assert.equal(count, 2);
-    assert.equal(LayerStore.getState().flatLayers.length, 1);
+    assert.equal(LayerStore.getState().flatLayers.length, 2);
   });
 
   it('listens to the app dispatcher', function() {
@@ -84,6 +90,21 @@ describe('LayerStore', function() {
     assert.equal(LayerStore.findLayer('1'), vector);
     LayerActions.removeLayer(vector);
     assert.equal(LayerStore.findLayer('1'), undefined);
+  });
+
+  it('handles layer order changes', function() {
+    LayerStore.bindMap(map);
+    var count = 0;
+    var onChangeCb = function() {
+      count++;
+    };
+    LayerStore.addChangeListener(onChangeCb);
+    var idx = level2.getLayers().getArray().indexOf(level3a);
+    assert.equal(idx, 1);
+    LayerActions.moveLayer(1, 0, level3a, level2);
+    assert.equal(count, 2);
+    idx = level2.getLayers().getArray().indexOf(level3a);
+    assert.equal(idx, 0);
   });
 
 });
