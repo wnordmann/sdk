f7499095dda783b9c392c8986ff8b00fda526ae6
Travis CI <bartvde@boundlessgeo.com>
2016-11-28 12:57:18 +0000

    Deploy to GitHub Pages: 893a362a85539e7bf9e92d035af406ab7c9d1840

--- dist/js/full.js
+++ dist/js/full.js
@@ -5,7 +5,7 @@
  * boundless-sdk-build:
  *   license: Apache-2.0 (http://opensource.org/licenses/Apache-2.0)
  *   author: Boundless Spatial, Inc.
- *   version: 0.10.2
+ *   version: 0.10.3
  * 
  * JSONSelect:
  *   author: Lloyd Hilaiel <lloyd@hilaiel.com>
