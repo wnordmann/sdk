7be18a2017d53bd22cbd123e5ea8d6a4a225ceb3
bartvde <bartvde@boundlessgeo.com>
2017-01-12 15:49:43 +0100

    Simplify touchTap tests, add one for Button

--- __tests__/components/Button.test.js
+++ __tests__/components/Button.test.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {assert} from 'chai';
+import TestUtils from 'react-addons-test-utils';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Button from '../../js/components/Button';
@@ -11,13 +12,19 @@ describe('Button', function() {
 
   it('button gets created', function() {
     var container = document.createElement('div');
+    var called = false;
+    var onClick = function() {
+      called = true;
+    };
     ReactDOM.render((
       <MuiThemeProvider muiTheme={getMuiTheme()}>
-        <Button label='My button' tooltip='foo' />
+        <Button onTouchTap={onClick} label='My button' tooltip='foo' />
       </MuiThemeProvider>
     ), container);
     var buttons = container.querySelectorAll('button');
     assert.equal(buttons.length, 1);
+    TestUtils.Simulate.touchTap(buttons[0]);
+    assert.equal(called, true);
     ReactDOM.unmountComponentAtNode(container);
   });
 
