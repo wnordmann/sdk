a3bbf2807987e8de37d2d82d88a231299e84639a
bartvde <bartvde@boundlessgeo.com>
2017-01-13 13:53:11 +0100

    Add extra test for button click in modal

--- __tests__/components/Button.test.js
+++ __tests__/components/Button.test.js
@@ -6,6 +6,7 @@ import {assert} from 'chai';
 import TestUtils from 'react-addons-test-utils';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Dialog from 'material-ui/Dialog';
 import Button from '../../js/components/Button';
 
 describe('Button', function() {
@@ -23,6 +24,26 @@ describe('Button', function() {
     ), container);
     var buttons = container.querySelectorAll('button');
     assert.equal(buttons.length, 1);
+    TestUtils.Simulate.touchTap(buttons[0]);
+    assert.equal(called, true);
+    ReactDOM.unmountComponentAtNode(container);
+  });
+
+  it('button in dialog calls onTouchTap', function() {
+    var container = document.createElement('div');
+    var called = false;
+    var onClick = function() {
+      called = true;
+    };
+    var actions = [
+      <Button onTouchTap={onClick} label='My button' tooltip='foo' />
+    ];
+    ReactDOM.render((
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <Dialog title='foo' actions={actions} modal={false} open={true} />
+      </MuiThemeProvider>
+    ), container);
+    var buttons = document.body.querySelectorAll('button');
     TestUtils.Simulate.touchTap(buttons[0]);
     assert.equal(called, true);
     ReactDOM.unmountComponentAtNode(container);
