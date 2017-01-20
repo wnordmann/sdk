960de268c4817e52950eb392a600a460bb085118
Willie Nordmann <wnordmann@boundlessgeo.com>
2017-01-20 14:00:37 -0600

    Adding in nuka-carousel

--- js/components/Bookmarks.js
+++ js/components/Bookmarks.js
@@ -15,19 +15,25 @@ import ol from 'openlayers';
 import Slider from 'react-slick';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
+import SvgIcon from 'material-ui/SvgIcon';
+import ArrowLeft from 'material-ui/svg-icons/hardware/keyboard-arrow-left';
+import ArrowRight from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
+
 import RaisedButton from 'material-ui/RaisedButton';
+import Carousel from 'nuka-carousel';
+
 import './Bookmarks.css';
-import './slick.css';
+// import './slick.css';
 import {defineMessages, injectIntl, intlShape} from 'react-intl';
 import classNames from 'classnames';
 import pureRender from 'pure-render-decorator';
 
 const messages = defineMessages({
-  dropdowntext: {
-    id: 'bookmarks.dropdowntext',
-    description: 'Text to use on the Bookmarks drop down',
-    defaultMessage: 'Bookmarks'
-  }
+    dropdowntext: {
+        id: 'bookmarks.dropdowntext',
+        description: 'Text to use on the Bookmarks drop down',
+        defaultMessage: 'Bookmarks'
+    }
 });
 
 /**
@@ -40,192 +46,293 @@ const messages = defineMessages({
  */
 @pureRender
 class Bookmarks extends React.Component {
-  static propTypes = {
-    /**
+    static propTypes = {
+        /**
      * The ol3 map instance on whose view we should navigate.
      */
-    map: React.PropTypes.instanceOf(ol.Map).isRequired,
-    /**
+        map: React.PropTypes.instanceOf(ol.Map).isRequired,
+        /**
      * Css class name to apply on the menu or the div.
      */
-    className: React.PropTypes.string,
-    /**
+        className: React.PropTypes.string,
+        /**
      * The bookmark data. An array of objects with name (string, required), description (string, required) and extent (array of number, required) keys.
      * The extent should be in the view projection.
      */
-    bookmarks: React.PropTypes.arrayOf(React.PropTypes.shape({
-      name: React.PropTypes.string.isRequired,
-      description: React.PropTypes.string.isRequired,
-      extent: React.PropTypes.arrayOf(React.PropTypes.number).isRequired
-    })).isRequired,
-    /**
+        bookmarks: React.PropTypes.arrayOf(React.PropTypes.shape({
+            name: React.PropTypes.string.isRequired,
+            description: React.PropTypes.string.isRequired,
+            extent: React.PropTypes.arrayOf(React.PropTypes.number).isRequired
+        })).isRequired,
+        /**
      * Should we show indicators? These are dots to navigate the bookmark pages.
      */
-    dots: React.PropTypes.bool,
-    /**
+        dots: React.PropTypes.bool,
+        /**
      * Should the scroller auto scroll?
      */
-    autoplay: React.PropTypes.bool,
-    /**
+        autoplay: React.PropTypes.bool,
+        /**
      * Delay between each auto scoll in ms.
      */
-    autoplaySpeed: React.PropTypes.number,
-    /**
+        autoplaySpeed: React.PropTypes.number,
+        /**
      * Should we animate the pan and zoom operation?
      */
-    animatePanZoom: React.PropTypes.bool,
-    /**
+        animatePanZoom: React.PropTypes.bool,
+        /**
      * The duration of the animation in milleseconds. Only relevant if animatePanZoom is true.
      */
-    animationDuration: React.PropTypes.number,
-    /**
+        animationDuration: React.PropTypes.number,
+        /**
      * The title on the introduction (first) page of the bookmarks.
      */
-    introTitle: React.PropTypes.string,
-    /**
+        introTitle: React.PropTypes.string,
+        /**
      * The description of the introduction (first) page of the bookmarks.
      */
-    introDescription: React.PropTypes.string,
-    /**
+        introDescription: React.PropTypes.string,
+        /**
      * @ignore
      */
-    intl: intlShape.isRequired,
-    /**
+        intl: intlShape.isRequired,
+        /**
      * Display as a menu drop down list.
      */
-    menu: React.PropTypes.bool,
-    /**
+        menu: React.PropTypes.bool,
+        /**
      * Should we display a marker for the bookmark?
      */
-    showMarker: React.PropTypes.bool,
-    /**
+        speed: React.PropTypes.number,
+        /**
+    *   Animation duration.
+    */
+        showMarker: React.PropTypes.bool,
+        /**
      * Url to the marker image to use for bookmark position.
      */
-    markerUrl: React.PropTypes.string
-  };
+        markerUrl: React.PropTypes.string,
+        /**
+     * Used to hardcode the slider width. Accepts any string dimension value such as "80%" or "500px".
+     */
+        width: React.PropTypes.string,
+        /**
+     * Sets infinite wrapAround mode. Defaults to true
+     */
+        wrapAround: React.PropTypes.bool
+    };
 
-  static defaultProps = {
-    style: {
-      margin: '10px 12px'
-    },
-    dots: true,
-    autoplay: false,
-    animatePanZoom: true,
-    introTitle: '',
-    introDescription: '',
-    animationDuration: 500,
-    menu: false,
-    showMarker: true,
-    markerUrl: './resources/marker.png'
-  };
+    static defaultProps = {
+        autoplay: false,
+        animatePanZoom: true,
+        introTitle: '',
+        introDescription: '',
+        animationDuration: 500,
+        menu: false,
+        showMarker: true,
+        markerUrl: './resources/marker.png',
+        wrapAround: true
+        width: '400px'
+    };
 
-  constructor(props) {
-    super(props);
-    var view = this.props.map.getView();
-    this._center = view.getCenter();
-    this._resolution = view.getResolution();
-    if (this._center === null) {
-      view.once('change:center', function(evt) {
-        this._center = evt.target.getCenter();
-      }, this);
+    constructor(props) {
+        super(props);
+        //this.props.speed = this.props.autoplaySpeed;
+        var view = this.props.map.getView();
+        this._center = view.getCenter();
+        this._resolution = view.getResolution();
+        if (this._center === null) {
+            view.once('change:center', function(evt) {
+                this._center = evt.target.getCenter();
+            }, this);
+        }
+        if (this._resolution === undefined) {
+            view.once('change:resolution', function(evt) {
+                this._resolution = evt.target.getResolution();
+            }, this);
+        }
+        this.state = {
+            value: null
+        }
     }
-    if (this._resolution === undefined) {
-      view.once('change:resolution', function(evt) {
-        this._resolution = evt.target.getResolution();
-      }, this);
+    componentDidMount() {
+        if (this.props.showMarker) {
+            this._layer = new ol.layer.Vector({
+                title: null,
+                managed: false,
+                style: new ol.style.Style({
+                    image: new ol.style.Icon({
+                        anchor: [
+                            0.5, 46
+                        ],
+                        anchorXUnits: 'fraction',
+                        anchorYUnits: 'pixels',
+                        opacity: 0.75,
+                        src: this.props.markerUrl
+                    })
+                }),
+                source: new ol.source.Vector({wrapX: false})
+            });
+            this.props.map.addLayer(this._layer);
+        }
     }
-    this.state = {
-      value: null
+    _handleChange(event, value) {
+        this.setState({value: value});
+        this._selectBookmark(value);
     }
-  }
-  componentDidMount() {
-    if (this.props.showMarker) {
-      this._layer = new ol.layer.Vector({
-        title: null,
-        managed: false,
-        style: new ol.style.Style({
-          image: new ol.style.Icon({
-            anchor: [0.5, 46],
-            anchorXUnits: 'fraction',
-            anchorYUnits: 'pixels',
-            opacity: 0.75,
-            src: this.props.markerUrl
-          })
-        }),
-        source: new ol.source.Vector({wrapX: false})
-      });
-      this.props.map.addLayer(this._layer);
+    _selectBookmark(bookmark) {
+        var map = this.props.map,
+            view = map.getView();
+        if (this.props.animatePanZoom === true) {
+            var pan = ol.animation.pan({duration: this.props.animationDuration, source: view.getCenter()});
+            var zoom = ol.animation.zoom({duration: this.props.animationDuration, resolution: view.getResolution(), source: view.getZoom()});
+            map.beforeRender(pan, zoom);
+        }
+        var center;
+        if (bookmark) {
+            var extent = bookmark.extent;
+            view.fit(extent, map.getSize());
+            center = ol.extent.getCenter(extent);
+        } else {
+            view.setCenter(this._center);
+            view.setResolution(this._resolution);
+            center = this._center;
+        }
+        if (this.props.showMarker) {
+            var source = this._layer.getSource();
+            source.clear();
+            var feature = new ol.Feature({geometry: new ol.geom.Point(center)});
+            source.addFeature(feature);
+        }
     }
-  }
-  _handleChange(event, value) {
-    this.setState({value: value});
-    this._selectBookmark(value);
-  }
-  _selectBookmark(bookmark) {
-    var map = this.props.map, view = map.getView();
-    if (this.props.animatePanZoom === true) {
-      var pan = ol.animation.pan({
-        duration: this.props.animationDuration,
-        source: view.getCenter()
-      });
-      var zoom = ol.animation.zoom({
-        duration: this.props.animationDuration,
-        resolution: view.getResolution(),
-        source: view.getZoom()
-      });
-      map.beforeRender(pan, zoom);
+    _afterChange(idx) {
+        var bookmark = idx === 0
+            ? false
+            : this.props.bookmarks[idx - 1];
+        this._selectBookmark(bookmark);
     }
-    var center;
-    if (bookmark) {
-      var extent = bookmark.extent;
-      view.fit(extent, map.getSize());
-      center = ol.extent.getCenter(extent);
-    } else {
-      view.setCenter(this._center);
-      view.setResolution(this._resolution);
-      center = this._center;
+    dots = React.createClass({
+        displayName: 'dots',
+
+        render: function render() {
+            var self = this;
+            var indexes = this.getIndexes(self.props.slideCount, self.props.slidesToScroll);
+            return React.createElement('ul', {
+                style: self.getListStyles()
+            }, indexes.map(function(index) {
+                return React.createElement('li', {
+                    style: self.getListItemStyles(),
+                    key: index
+                }, React.createElement('button', {
+                    style: self.getButtonStyles(self.props.currentSlide === index),
+                    onClick: self.props.goToSlide.bind(null, index)
+                }, '•'));
+            }));
+        },
+        getIndexes: function getIndexes(count, inc) {
+            var arr = [];
+            for (var i = 0; i < count; i += inc) {
+                arr.push(i);
+            }
+            return arr;
+        },
+        getListStyles: function getListStyles() {
+            return {position: 'relative', margin: 0,  padding: 0, top: '38px'};
+        },
+        getListItemStyles: function getListItemStyles() {
+            return {listStyleType: 'none', display: 'inline-block'};
+        },
+        getButtonStyles: function getButtonStyles(active) {
+            return {
+                border: 0,
+                background: 'transparent',
+                color: 'black',
+                cursor: 'pointer',
+                padding: 10,
+                outline: 0,
+                fontSize: 24,
+                opacity: active
+                    ? 1
+                    : 0.5
+            };
+        }
+    });
+
+
+    _decorator = [
+        {
+            component: React.createClass({
+                render() {
+                    return (<ArrowLeft onClick={this.props.previousSlide}/>)
+                }
+            }),
+            position: 'CenterLeft'
+        }, {
+            component: React.createClass({
+                render() {
+                    return (<ArrowRight onClick={this.props.nextSlide}/>)
+                }
+            }),
+            position: 'CenterRight'
+        }
+    ];
+
+    render() {
+        const {formatMessage} = this.props.intl;
+        let Decorators = this._decorator;
+
+        if (this.props.menu === true) {
+            var menuChildren = this.props.bookmarks.map(function(bookmark) {
+                return (<MenuItem key={bookmark.name} value={bookmark.name} primaryText={bookmark.name}/>);
+            }, this);
+            return (
+                <IconMenu anchorOrigin={{
+                    horizontal: 'left',
+                    vertical: 'top'
+                }} targetOrigin={{
+                    horizontal: 'left',
+                    vertical: 'top'
+                }} className={classNames('sdk-component story-panel', this.props.className)} iconButtonElement={< RaisedButton label = {
+                    formatMessage(messages.dropdowntext)
+                } />} value={this.state.value} onChange={this._handleChange.bind(this)}>
+                    {menuChildren}
+                </IconMenu>
+            );
+        } else {
+            var getHTML = function(bookmark) {
+                return {__html: bookmark.description};
+            };
+            var carouselChildren = this.props.bookmarks.map(function(bookmark) {
+                return (
+                    <div key={bookmark.name}>
+                        <h2>{bookmark.name}</h2>
+                        <p dangerouslySetInnerHTML={getHTML(bookmark)}></p>
+                    </div>
+                );
+            });
+            carouselChildren.unshift(
+                <div key='intro'>
+                    <h2>{this.props.introTitle}</h2>
+                    <p>{this.props.introDescription}</p>
+                </div>
+            );
+            if(this.props.dots){
+              Decorators.push({component:this.dots, position:'BottomCenter'});
+            }
+            return (
+                <div className={classNames('sdk-component story-panel', this.props.className)}>
+                    <Carousel {...this.props}
+                      decorators={Decorators}
+                      arrows={true}
+                      afterChange={this._afterChange.bind(this)}
+                      framePadding="0px 20px 38px 20px"
+                      initialSlideHeight={401}>
+                        {carouselChildren}
+                    </Carousel>
+                </div>
+            );
+        }
     }
-    if (this.props.showMarker) {
-      var source = this._layer.getSource();
-      source.clear();
-      var feature = new ol.Feature({
-        geometry: new ol.geom.Point(center)
-      });
-      source.addFeature(feature);
-    }
-  }
-  _afterChange(idx) {
-    var bookmark = idx === 0 ? false : this.props.bookmarks[idx - 1];
-    this._selectBookmark(bookmark);
-  }
-  render() {
-    const {formatMessage} = this.props.intl;
-    if (this.props.menu === true) {
-      var menuChildren = this.props.bookmarks.map(function(bookmark) {
-        return (<MenuItem key={bookmark.name} value={bookmark.name} primaryText={bookmark.name}/>);
-      }, this);
-      return (
-        <IconMenu anchorOrigin={{horizontal: 'left', vertical: 'top'}} targetOrigin={{horizontal: 'left', vertical: 'top'}} className={classNames('sdk-component story-panel', this.props.className)} iconButtonElement={<RaisedButton label={formatMessage(messages.dropdowntext)} />} value={this.state.value} onChange={this._handleChange.bind(this)}>
-          {menuChildren}
-        </IconMenu>
-      );
-    } else {
-      var getHTML = function(bookmark) {
-        return {__html: bookmark.description};
-      };
-      var carouselChildren = this.props.bookmarks.map(function(bookmark) {
-        return (<div key={bookmark.name}><h2>{bookmark.name}</h2><p dangerouslySetInnerHTML={getHTML(bookmark)}></p></div>);
-      });
-      carouselChildren.unshift(<div key='intro'><h2>{this.props.introTitle}</h2><p>{this.props.introDescription}</p></div>);
-      return (
-        <div className={classNames('sdk-component story-panel', this.props.className)}>
-          <Slider {...this.props} arrows={true} afterChange={this._afterChange.bind(this)}>
-            {carouselChildren}
-          </Slider>
-        </div>
-      );
-    }
-  }
 }
 
 export default injectIntl(Bookmarks);
