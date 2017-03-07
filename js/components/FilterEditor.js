f479607287bf06d9faee2f9777e9b1fa36a725f5
Jeremy Mulenex <jeremy@aboutjm.com>
2017-03-06 20:45:50 -0600

    fixed travis error

--- js/components/FilterEditor.js
+++ js/components/FilterEditor.js
@@ -11,7 +11,7 @@
  */
 
 import React from 'react';
-import { List, ListItem } from 'material-ui/List';
+import {List, ListItem} from 'material-ui/List';
 import MenuItem from 'material-ui/MenuItem';
 import Subheader from 'material-ui/Subheader';
 import Paper from 'material-ui/Paper';
