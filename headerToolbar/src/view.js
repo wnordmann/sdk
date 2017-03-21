3dbb0b25625a704b17056188738b66452e9649db
Jeremy Mulenex <jeremy@aboutjm.com>
2017-03-21 10:38:08 -0500

    added the rest of the icons and the popover menu

--- headerToolbar/src/view.js
+++ headerToolbar/src/view.js
@@ -6,6 +6,8 @@ import 'font-awesome-webpack';
 import Divider from 'material-ui/Divider';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -16,6 +18,30 @@ import AppBar from 'material-ui/AppBar';
 // require('./mapskin/fonts/mapskin.css');
 
 export default class DrawerSimpleExample extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleTouchTap = (event) => {
+    // This prevents ghost click.
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget,
+    });
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
 
 
 
@@ -28,13 +54,27 @@ export default class DrawerSimpleExample extends React.Component {
           iconElementRight={
             <div className="headerIcons">
               <IconButton
-                 iconClassName="headerIcons ms ms-measure-distance"
+                 iconClassName="headerIcons ms ms-select-box"
                 />
               <IconButton
                  iconClassName="headerIcons ms ms-measure-distance"
                 />
-              <IconButton>
+              <IconButton onTouchTap={this.handleTouchTap} >
                 <NavigationMoreVert />
+                <Popover
+                  open={this.state.open}
+                  anchorEl={this.state.anchorEl}
+                  anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+                  targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                  onRequestClose={this.handleRequestClose}
+
+                >
+                  <Menu>
+                    <MenuItem primaryText="Load" />
+                    <MenuItem primaryText="Save" />
+                    <MenuItem primaryText="Login" />
+                  </Menu>
+                </Popover>
               </IconButton>
             </div>}
         />
