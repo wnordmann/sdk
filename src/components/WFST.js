89f891b5fc897cf0df9f99bc29b8d2ac93785542
bartvde <bartvde@boundlessgeo.com>
2017-03-22 11:15:55 +0100

    Set value prop on LayerSelector in WFST

--- src/components/WFST.js
+++ src/components/WFST.js
@@ -469,8 +469,12 @@ class WFST extends React.PureComponent {
       }
       var layerSelector;
       if (this.props.layerSelector) {
+        var id;
+        if (this.state.layer) {
+          id = this.state.layer.get('id');
+        }
         layerSelector = (
-          <LayerSelector disabled={this.state.disabled || !this.state.layer} style={{marginLeft: '36px'}} {...this.props} onChange={this._onLayerSelectChange.bind(this)} id='layerSelector' ref='layerSelector' filter={this._filterLayerList} map={this.props.map} />
+          <LayerSelector value={id} disabled={this.state.disabled || !this.state.layer} style={{marginLeft: '36px'}} {...this.props} onChange={this._onLayerSelectChange.bind(this)} id='layerSelector' ref='layerSelector' filter={this._filterLayerList} map={this.props.map} />
         );
       } else if (this.state.layer) {
         var label = formatMessage(messages.layerlabel) + ': ' + this.state.layer.get('title');
