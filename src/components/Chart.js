b6d9a0a51d13015cc30a0942855b2627fbc52b02
Carly Langlois <cj.langlois.cl@gmail.com>
2017-05-25 09:45:18 -0500

    adding chart docs, check for link to QueryBuilder component

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -44,32 +44,10 @@ const messages = defineMessages({
     defaultMessage: 'Feature count'
   }
 });
-
-/**
- * Allow for the creation of charts based on selected features of a layer.
- *
- * ```javascript
- * var charts = [{
- *   title: 'Airports count per use category',
- *   categoryField: 'USE',
- *   layer: 'lyr03',
- *   valueFields: [],
- *   displayMode: 2,
- *   operation: 2
- * }, {
- *   title: 'Forest area total surface',
- *   categoryField: 'VEGDESC',
- *   layer: 'lyr01',
- *   valueFields: ['AREA_KM2'],
- *   displayMode: 1,
- *   operation: 2
- * }];
- * ```
- *
- * ```xml
- * <Chart ref='chartPanel' charts={charts}/>
- * ```
- */
+ /**
+  * $$src/components/InfoPopupDetail.md$$
+  *
+  */
 class Chart extends React.Component {
   static propTypes = {
     /**
@@ -84,13 +62,23 @@ class Chart extends React.Component {
 
      * valueFields (array of string, required) is an array of field names to use for displaying values in the chart.
 
-     * displayMode (enum(0, 1, 2), required) defines how the feature attributes will be used to create the chart. When using a value of 0 (by feature), an element will
-     * be added to the chart for each selected feature. When using a value of 1 (by category), selected features will be grouped according to
-     * a category defined by the categoryField. When using a value of 2 (count by category) the chart will show the number of features in each
-     * category.
+     * displayMode (enum(0, 1, 2), required) defines how the feature attributes will be used to create the chart.
+
+            When using a value of 0 (by feature), an element will be added to the chart for each selected feature.
+
+            When using a value of 1 (by category), selected features will be grouped according to a category defined by the categoryField.
+
+            When using a value of 2 (count by category) the chart will show the number of features in each category.
 
      * The statistic function to use when displayMode is by category (1) is defined in the operation (enum(0, 1, 2, 3), optional) key.
-     * A value of 0 means MIN, a value of 1 means MAX, a value of 2 means SUM and a value of 3 means AVG (Average).
+
+            A value of 0 means MIN,
+
+            a value of 1 means MAX,
+
+            a value of 2 means SUM
+
+            and a value of 3 means AVG (Average).
      */
     charts: React.PropTypes.arrayOf(React.PropTypes.shape({
       title: React.PropTypes.string.isRequired,
