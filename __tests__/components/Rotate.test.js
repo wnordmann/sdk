ea683ba7156e0df329fd1b6e092e165e44a466ee
Dan 'Ducky' Little <theduckylittle@gmail.com>
2017-06-13 15:49:21 -0500

    Finished tests for Rotate.
    Also included missed changes to the Map component.

--- __tests__/components/Rotate.test.js
+++ __tests__/components/Rotate.test.js
@@ -3,75 +3,51 @@
 import TestUtils from 'react-addons-test-utils';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {assert} from 'chai';
-import ol from 'openlayers';
+import {expect} from 'chai';
 import intl from '../mock-i18n';
+import {addLocaleData, IntlProvider} from 'react-intl';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import 'phantomjs-polyfill-object-assign';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import BoundlessSdk from '../../src/components/BoundlessSdk';
 import Rotate from '../../src/components/Rotate';
+import configureStore from '../../src/stores/Store';
 
 import polyfills from '../polyfills';
 
+import * as mapActions from '../../src/actions/MapActions';
+
 describe('Rotate', function() {
-  var target, map;
   var width = 360;
   var height = 180;
 
-  beforeEach(function(done) {
-    target = document.createElement('div');
-    var style = target.style;
-    style.position = 'absolute';
-    style.left = '-1000px';
-    style.top = '-1000px';
-    style.width = width + 'px';
-    style.height = height + 'px';
-    document.body.appendChild(target);
-    map = new ol.Map({
-      target: target,
-      view: new ol.View({
-        center: [0, 0],
-        zoom: 1,
-        rotation: 0.1
-      })
-    });
-    map.once('postrender', function() {
-      done();
-    });
-  });
-
-  afterEach(function() {
-    map.setTarget(null);
-    document.body.removeChild(target);
-  });
-
-  it('rotates the map back to north', function(done) {
+  it('rotates the map back to north', function() {
     var container = document.createElement('div');
+    const store = configureStore();
+
+    // render the rotate button
     ReactDOM.render((
-      <BoundlessSdk>
-        <MuiThemeProvider muiTheme={getMuiTheme()}>
-          <Rotate intl={intl} map={map} />
-        </MuiThemeProvider>
-      </BoundlessSdk>
+      <div>
+        <IntlProvider locale="en">
+          <BoundlessSdk store={store}>
+            <MuiThemeProvider muiTheme={getMuiTheme()}>
+              <Rotate intl={intl} />
+            </MuiThemeProvider>
+          </BoundlessSdk>
+        </IntlProvider>
+      </div>
     ), container);
+
+    store.dispatch(mapActions.setRotation(0.1));
+    expect(store.getState().mapState.view.rotation).to.equal(0.1);
+
     var buttons = container.querySelectorAll('button');
     var rotate = buttons[0];
-    assert.equal(map.getView().getRotation(), 0.1);
     TestUtils.Simulate.touchTap(rotate);
-    window.setTimeout(function() {
-      assert.equal(Math.round(map.getView().getRotation()), 0);
-      ReactDOM.unmountComponentAtNode(container);
-      done();
-    }, 1000);
-  });
 
-  it('renders the rotate component', function() {
-    const renderer = TestUtils.createRenderer();
-    renderer.render(<Rotate intl={intl} map={map} autoHide={false}/>);
-    const actual = renderer.getRenderOutput().props.className;
-    const expected = 'sdk-component rotate';
-    assert.equal(actual, expected);
+    expect(store.getState().mapState.view.rotation).to.equal(0);
+
+    ReactDOM.unmountComponentAtNode(container);
   });
 
 });
