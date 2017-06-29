3cc07e8f2ddcd52e972c73022c4d3904e6cc20fe
Willie Nordmann <wnordmann@boundlessgeo.com>
2017-06-29 14:51:54 -0500

    Updating to use redux chrome extention

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -15,7 +15,7 @@ import * as mapActions from '@boundlessgeo/sdk/actions/map';
 
 const store = createStore(combineReducers({
   map: SdkMapReducer,
-}));
+}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 window.store = store;
 
