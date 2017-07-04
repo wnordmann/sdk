1b7e63b98c4a6265d889bab47e2d03ed913f0b65
Dan 'Ducky' Little <theduckylittle@gmail.com>
2017-07-04 12:36:12 -0500

    Increased coverage of map component

--- __tests__/components/map.test.js
+++ __tests__/components/map.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 
+
 import { Map } from '../../src/components/map';
 import olMap from 'ol/map';
 import TileLayer from 'ol/layer/tile';
+
+import { createStore, combineReducers } from 'redux'
+import ConnectedMap from '../../src/components/map';
+import MapReducer from '../../src/reducers/map';
 
 describe('Map component', () => {
 
@@ -22,20 +27,42 @@ describe('Map component', () => {
           "https://b.tile.openstreetmap.org/{z}/{x}/{y}.png",
           "https://c.tile.openstreetmap.org/{z}/{x}/{y}.png"
         ]
+      },
+      "states-wms": {
+        "type": "raster",
+        "tileSize": 256,
+        "tiles": ["https://ahocevar.com/geoserver/gwc/service/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image/png&SRS=EPSG:900913&LAYERS=topp:states&STYLES=&WIDTH=256&HEIGHT=256&BBOX={bbox-epsg-3857}"]
       }
     };
     const layers = [{
       "id": "osm",
       "source": "osm"
+    }, {
+      "id": "states",
+      "source": "states-wms",
     }];
     const center = [0, 0];
     const zoom = 2;
-    const wrapper = shallow(<Map map={{center, zoom, sources, layers}} />);
+    const _sourcesVersion = 0, _layersVersion = 0;
+    const wrapper = shallow(<Map map={{center, zoom, sources, layers, _sourcesVersion, _layersVersion}} />);
     wrapper.instance().componentDidMount();
     const map = wrapper.instance().map;
     expect(map).toBeDefined();
     expect(map).toBeInstanceOf(olMap);
     expect(map.getLayers().item(0)).toBeInstanceOf(TileLayer);
+
+    // move the map.
+    wrapper.setProps({
+      zoom: 4
+    });
+  });
+
+  it('should created a connected map', function() {
+    const store = createStore(combineReducers({
+      map: MapReducer,
+    }));
+
+    const wrapper = mount(<ConnectedMap store={store}/>);
   });
 
 });
