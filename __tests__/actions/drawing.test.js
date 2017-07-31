ad2268cb3f6fa0a66ca13f9569995c8258f0a655
bartvde <bartvde@boundlessgeo.com>
2017-07-31 09:56:30 +0200

    Complete test coverage for drawing actions

--- __tests__/actions/drawing.test.js
+++ __tests__/actions/drawing.test.js
@@ -2,6 +2,7 @@
 
 import * as actions from '../../src/actions/drawing';
 import { DRAWING } from '../../src/action-types';
+import { INTERACTIONS } from '../../src/constants';
 
 describe('drawing actions', () => {
   it('should create an action to start drawing', () => {
@@ -16,7 +17,38 @@ describe('drawing actions', () => {
     expect(actions.startDrawing(source, geo_type)).toEqual(expectedAction);
   });
 
+  it('should create an action to start modifying', () => {
+    const source = 'test-points';
+
+    const expectedAction = {
+      type: DRAWING.START,
+      interaction: INTERACTIONS.modify,
+      sourceName: source,
+    };
+    expect(actions.startModify(source)).toEqual(expectedAction);
+  });
+
+  it('should create an action to start select', () => {
+    const source = 'test-points';
+
+    const expectedAction = {
+      type: DRAWING.START,
+      interaction: INTERACTIONS.select,
+      sourceName: source,
+    };
+    expect(actions.startSelect(source)).toEqual(expectedAction);
+  });
+
   it('should create an action to end drawing', () => {
     expect(actions.endDrawing()).toEqual({ type: DRAWING.END });
   });
+
+  it('should create an action to end modify', () => {
+    expect(actions.endModify()).toEqual({ type: DRAWING.END });
+  });
+
+  it('should create an action to end select', () => {
+    expect(actions.endSelect()).toEqual({ type: DRAWING.END });
+  });
+
 });
