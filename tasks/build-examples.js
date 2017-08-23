c742f09e042be41884b4943fe843316de9625a35
bartvde <bartvde@boundlessgeo.com>
2017-08-23 10:10:47 +0200

    Lint the tasks files as well

--- tasks/build-examples.js
+++ tasks/build-examples.js
@@ -1,20 +1,20 @@
-var path = require('path');
+const path = require('path');
 
-var Metalsmith = require('metalsmith');
-var handlebars = require('handlebars');
-var templates = require('metalsmith-layouts');
-var marked = require('marked');
+const Metalsmith = require('metalsmith');
+const handlebars = require('handlebars');
+const templates = require('metalsmith-layouts');
+const marked = require('marked');
 
-var srcDir = path.join(__dirname, '..', 'examples');
-var destDir = path.join(__dirname, '..', 'build', 'examples');
-var templatesDir = path.join(__dirname, '..', 'config', 'examples');
+const srcDir = path.join(__dirname, '..', 'examples');
+const destDir = path.join(__dirname, '..', 'build', 'examples');
+const templatesDir = path.join(__dirname, '..', 'config', 'examples');
 
-var isJsRegEx = /\.js$/;
-var isCssRegEx = /\.css$/;
+const isJsRegEx = /\.js$/;
+const isCssRegEx = /\.css$/;
 
 function createIndex(files, metalsmith, done) {
   setImmediate(done); // all remaining code is synchronous
-  var index = '<!DOCTYPE html>';
+  let index = '<!DOCTYPE html>';
   index += '<html>';
   index += '<head>';
   index += '<title>Boundless SDK Examples</title>';
@@ -25,8 +25,10 @@ function createIndex(files, metalsmith, done) {
   index += 'Boundless SDK Examples';
   index += '</div>';
   index += '<ul>';
-  for (var filename in files) {
-    var example = files[filename];
+  const keys = Object.keys(files);
+  for (let i = 0, ii = keys.length; i < ii; ++i) {
+    const filename = keys[i];
+    const example = files[filename];
     if (filename.indexOf('index.html') !== -1 && filename.indexOf('.swp') === -1) {
       index += '<li>';
       index += `<a href="${filename}">${example.title}</a><br>`;
@@ -37,26 +39,29 @@ function createIndex(files, metalsmith, done) {
   index += '</ul>';
   index += '</body>';
   index += '</html>';
+  // eslint-disable-next-line no-param-reassign
   files['index.html'] = {
     contents: new Buffer(index),
-    mode: '0644'
+    mode: '0644',
   };
 }
 
 function augmentExamples(files, metalsmith, done) {
   setImmediate(done); // all remaining code is synchronous
-  for (var filename in files) {
-    var file = files[filename];
+  const keys = Object.keys(files);
+  for (let i = 0, ii = keys.length; i < ii; ++i) {
+    const filename = keys[i];
+    const file = files[filename];
     if (filename.indexOf('index.html') !== -1 && filename.indexOf('.swp') === -1) {
       if (!file.layout) {
-        throw new Error(filename + ': Missing "layout" in YAML front-matter');
+        throw new Error(`${filename}: Missing "layout" in YAML front-matter`);
       }
-      var id = filename.split(path.sep)[0];
+      const id = filename.split(path.sep)[0];
 
       file.js = {
-        tag: '<script src="./' + id + '.bundle.js"></script>',
+        tag: `<script src="./${id}.bundle.js"></script>`,
       };
-      var cssFilename = id + '/app.css';
+      const cssFilename = `${id}/app.css`;
       if (cssFilename in files) {
         file.css = {
           tag: '<link rel="stylesheet" href="app.css">',
@@ -65,16 +70,15 @@ function augmentExamples(files, metalsmith, done) {
 
       // add additional resources
       if (file.resources) {
-        var resources = [];
-        for (var i = 0, ii = file.resources.length; i < ii; ++i) {
-          var resource = file.resources[i];
+        const resources = [];
+        for (let j = 0, jj = file.resources.length; j < jj; ++j) {
+          const resource = file.resources[j];
           if (isJsRegEx.test(resource)) {
-            resources[i] = '<script src="' + resource + '"></script>';
+            resources[j] = `<script src="${resource}"></script>`;
           } else if (isCssRegEx.test(resource)) {
-            resources[i] = '<link rel="stylesheet" href="' + resource + '">';
+            resources[j] = `<link rel="stylesheet" href="${resource}">`;
           } else {
-            throw new Error('Invalid value for resource: ' +
-                resource + ' is not .js or .css: ' + filename);
+            throw new Error(`Invalid value for resource: ${resource} is not .js or .css: ${filename}`);
           }
         }
         file.extraHead = {
@@ -86,7 +90,7 @@ function augmentExamples(files, metalsmith, done) {
 }
 
 function main(callback) {
-  var smith = new Metalsmith('.')
+  const smith = new Metalsmith('.')
       .source(srcDir)
       .destination(destDir)
       .concurrency(25)
@@ -96,23 +100,21 @@ function main(callback) {
         engine: 'handlebars',
         directory: templatesDir,
         helpers: {
-          md: function(str) {
+          md(str) {
             return new handlebars.SafeString(marked(str));
           },
         },
       }))
-      .build(function(err) {
+      .build((err) => {
         callback(err);
       });
   return smith;
 }
 
 if (require.main === module) {
-  main(function(err) {
+  main((err) => {
     if (err) {
-      process.stderr.write(
-          'Building examples failed.  See the full trace below.\n\n' +
-          err.stack + '\n');
+      process.stderr.write(`Building examples failed.  See the full trace below.\n\n ${err.stack} \n`);
       process.exit(1);
     } else {
       process.exit(0);
