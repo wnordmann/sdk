8edf861fcb830c2540d05f524f43267fba902496
Willie Nordmann <wnordmann@boundlessgeo.com>
2017-09-01 10:25:44 -0500

    Adding in missing files

---
+++ examples/bookmark/moveButton.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import * as bookmarkAction from './action';
+import * as mapActions from '@boundlessgeo/sdk/actions/map';
+
+// Custom Bookmark Component
+class MoveButtonComponent extends React.PureComponent{
+	// This is the where action really happens, update state and move the map
+	moveBookmark(count){
+		this.props.moveSlide(count);
+		const feature = this.props.map.sources[this.props.bookmarkSource].data.features[count];
+		this.props.zoomTo(feature.geometry.coordinates, 18);
+	}
+	// Logic for handling next button
+	nextBookmark(){
+		const featureCount = this.props.map.sources[this.props.bookmarkSource].data.features.length;
+		const currentCount = this.props.bookmark.count;
+		const newCount  = currentCount >= featureCount - 1 ? 0 : currentCount + 1;
+		this.moveBookmark(newCount);
+	}
+	// Logic for handling previous button
+	previousBookmark(){
+		const featureCount = this.props.map.sources[this.props.bookmarkSource].data.features.length;
+		const currentCount = this.props.bookmark.count;
+		const newCount = currentCount <= 0 ? featureCount - 1 : currentCount - 1;
+		this.moveBookmark(newCount);
+	}
+	// Render the buttons
+	render() {
+		return (
+			<span>
+				<button className="sdk-btn" onClick={() => { this.previousBookmark() }}  >Previous</button> <button className="sdk-btn" onClick={() => {this.nextBookmark()}}>Next</button>
+			</span>
+		)
+	}
+}
+// Getting the bookmark and map stores
+function mapStateToProps(state) {
+  return {
+    bookmark: state.bookmark,
+    map: state.map,
+  };
+}
+// Need the moveSlide function from bookmark reducer
+// Need setView function from map reducer
+function mapDispatchToProps(dispatch) {
+  return {
+    moveSlide: (count) => {
+      dispatch(bookmarkAction.moveSlide(count));
+    },
+		zoomTo: (coords, zoomLevel) => {
+			dispatch(mapActions.setView(coords, zoomLevel));
+		}
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(MoveButtonComponent);
