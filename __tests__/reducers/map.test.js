4be940516f9a341d7fd2be554823de480b5531e2
bartvde <bartvde@boundlessgeo.com>
2017-11-28 18:12:18 +0100

    Add another test case for when no metadata present

--- __tests__/reducers/map.test.js
+++ __tests__/reducers/map.test.js
@@ -250,6 +250,34 @@ describe('map reducer', () => {
     expect(reducer(state, action)).toEqual(context);
   });
 
+  it('should handle RECEIVE_CONTEXT with no metadata in context', () => {
+    const state = {
+      version: 8,
+      name: 'default',
+      center: [0, 0],
+      zoom: 3,
+      sources: {},
+      layers: [],
+    };
+    deepFreeze(state);
+    const context = {
+      version: 9,
+      name: 'foo',
+      center: [10, 10],
+      zoom: 4,
+      metadata: {
+        'bnd:layer-version': 1,
+        'bnd:source-version': 1,
+      },
+    };
+    const action = {
+      type: MAP.RECEIVE_CONTEXT,
+      context,
+    };
+    deepFreeze(action);
+    expect(reducer(state, action)).toEqual(context);
+  });
+
   it('should handle SET_LAYER_METADATA', () => {
     const title = 'Background';
     const layer = {
