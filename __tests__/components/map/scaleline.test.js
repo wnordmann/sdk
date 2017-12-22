30204014b5018bee58809069ac9007868a0c3d02
bartvde <bartvde@boundlessgeo.com>
2017-12-22 12:55:07 +0100

    Add unit tests for scaleline

---
+++ __tests__/components/map/scaleline.test.js
@@ -0,0 +1,175 @@
+/* global it, describe, expect, beforeEach */
+
+import React from 'react';
+import {mount, configure} from 'enzyme';
+import  Adapter from 'enzyme-adapter-react-16';
+
+import {createStore, combineReducers} from 'redux';
+
+import MapReducer from '../../../src/reducers/map';
+import MapInfoReducer from '../../../src/reducers/mapinfo';
+import * as MapInfoActions from '../../../src/actions/mapinfo';
+import * as MapActions from '../../../src/actions/map';
+import ConnectedScaleLine, {ScaleLine} from '../../../src/components/map/scaleline';
+
+configure({adapter: new Adapter()});
+
+describe('Scaleline tests', () => {
+
+  let store;
+
+  beforeEach(() => {
+    store = createStore(combineReducers({
+      map: MapReducer,
+      mapinfo: MapInfoReducer,
+    }));
+  });
+
+  it('create the correct scaleline', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:3857'));
+    store.dispatch(MapInfoActions.setResolution(1000));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const wrapper = mount(<ConnectedScaleLine store={store} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('creates the meter suffix', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:3857'));
+    store.dispatch(MapInfoActions.setResolution(2.388657133911758));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const wrapper = mount(<ConnectedScaleLine store={store} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('creates the millimeter suffix', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:3857'));
+    store.dispatch(MapInfoActions.setResolution(0.009330691929342804));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const wrapper = mount(<ConnectedScaleLine store={store} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('creates the micrometer suffix', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:3857'));
+    store.dispatch(MapInfoActions.setResolution(0.000009330691929342804));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const wrapper = mount(<ConnectedScaleLine store={store} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('create the correct scaleline in EPSG:4326', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:4326'));
+    store.dispatch(MapInfoActions.setResolution(1000));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const wrapper = mount(<ConnectedScaleLine units='degrees' store={store} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('create the correct scaleline in degrees', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:3857'));
+    store.dispatch(MapInfoActions.setResolution(19567.87924100512));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const wrapper = mount(<ConnectedScaleLine units='degrees' store={store} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('create the correct scaleline in degrees (minutes)', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:3857'));
+    store.dispatch(MapInfoActions.setResolution(300));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const wrapper = mount(<ConnectedScaleLine units='degrees' store={store} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('create the correct scaleline in degrees (seconds)', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:3857'));
+    store.dispatch(MapInfoActions.setResolution(1));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const wrapper = mount(<ConnectedScaleLine units='degrees' store={store} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('create the correct scaleline in imperial', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:3857'));
+    store.dispatch(MapInfoActions.setResolution(1000));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const wrapper = mount(<ConnectedScaleLine units='imperial' store={store} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('create the correct scaleline in imperial (ft)', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:3857'));
+    store.dispatch(MapInfoActions.setResolution(1));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const wrapper = mount(<ConnectedScaleLine units='imperial' store={store} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('create the correct scaleline in imperial (in)', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:3857'));
+    store.dispatch(MapInfoActions.setResolution(0.001));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const wrapper = mount(<ConnectedScaleLine units='imperial' store={store} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('create the correct scaleline in nautical', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:3857'));
+    store.dispatch(MapInfoActions.setResolution(1000));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const wrapper = mount(<ConnectedScaleLine units='nautical' store={store} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('create the correct scaleline in us', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:3857'));
+    store.dispatch(MapInfoActions.setResolution(1000));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const wrapper = mount(<ConnectedScaleLine units='us' store={store} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('create the correct scaleline in us (ft)', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:3857'));
+    store.dispatch(MapInfoActions.setResolution(1));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const wrapper = mount(<ConnectedScaleLine units='us' store={store} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('create the correct scaleline in us (in)', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:3857'));
+    store.dispatch(MapInfoActions.setResolution(0.001));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const wrapper = mount(<ConnectedScaleLine units='us' store={store} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('shouldComponentUpdate should work correctly', () => {
+    store.dispatch(MapInfoActions.setProjection('EPSG:3857'));
+    store.dispatch(MapInfoActions.setResolution(1000));
+    store.dispatch(MapActions.setView([-98, 40], 4));
+    const props = {
+      map: {
+        center: [-98, 40],
+        zoom: 4,
+      },
+      mapinfo: {
+        projection: 'EPSG:3857',
+        resolution: 1000,
+      }
+    };
+    const wrapper = mount(<ScaleLine {...props} />);
+    const scaleline = wrapper.instance();
+    let result;
+    result = scaleline.shouldComponentUpdate({mapinfo: {resolution: 1000}});
+    expect(result).toBe(false);
+    result = scaleline.shouldComponentUpdate({units: 'degrees'});
+    expect(result).toBe(true);
+    result = scaleline.shouldComponentUpdate({mapinfo: {resolution: 500}});
+    expect(result).toBe(true);
+    result = scaleline.shouldComponentUpdate({map: {center: [-98, 50]}});
+    expect(result).toBe(true);
+  });
+
+});
--- src/components/map/scaleline.js
+++ src/components/map/scaleline.js
@@ -28,10 +28,10 @@ const LEADING_DIGITS = [1, 2, 5];
  *
  * @desc Provides a scale line control.
  */
-class ScaleLine extends React.Component {
+export class ScaleLine extends React.Component {
   shouldComponentUpdate(nextProps) {
     // compare center
-    if (this.props.center && nextProps.center && ((nextProps.center[0] !== this.props.center[0] || nextProps.center[1] !== this.props.center[1]))) {
+    if (this.props.map && nextProps.map && this.props.map.center && nextProps.map.center && ((nextProps.map.center[0] !== this.props.map.center[0] || nextProps.map.center[1] !== this.props.map.center[1]))) {
       return true;
     }
     // compare resolution
@@ -39,79 +39,88 @@ class ScaleLine extends React.Component {
       return true;
     }
     // compare units
-    if (nextProps.units !== this.props.units) {
+    if (nextProps.units !== undefined && nextProps.units !== this.props.units) {
       return true;
     }
     return false;
   }
+  calculateProperties() {
+    const center = Proj.fromLonLat(this.props.map.center);
+    const units = this.props.units;
+    const pointResolutionUnits = units === DEGREES ? DEGREES : 'm';
+    const projection = Proj.get(this.props.mapinfo.projection);
+    const resolution = this.props.mapinfo.resolution;
+    let pointResolution = Proj.getPointResolution(projection, resolution, center, pointResolutionUnits);
+    if (units !== DEGREES) {
+      pointResolution *= projection.getMetersPerUnit();
+    }
+    let nominalCount = this.props.minWidth * pointResolution;
+    let suffix = '';
+    if (units === DEGREES) {
+      const metersPerDegree = Proj.METERS_PER_UNIT[DEGREES];
+      if (projection.getUnits() === DEGREES) {
+        nominalCount *= metersPerDegree;
+      } else {
+        pointResolution /= metersPerDegree;
+      }
+      if (nominalCount < metersPerDegree / 60) {
+        suffix = '\u2033'; // seconds
+        pointResolution *= 3600;
+      } else if (nominalCount < metersPerDegree) {
+        suffix = '\u2032'; // minutes
+        pointResolution *= 60;
+      } else {
+        suffix = '\u00b0'; // degrees
+      }
+    } else if (units === IMPERIAL) {
+      if (nominalCount < 0.9144) {
+        suffix = 'in';
+        pointResolution /= 0.0254;
+      } else if (nominalCount < 1609.344) {
+        suffix = 'ft';
+        pointResolution /= 0.3048;
+      } else {
+        suffix = 'mi';
+        pointResolution /= 1609.344;
+      }
+    } else if (units === NAUTICAL) {
+      pointResolution /= 1852;
+      suffix = 'nm';
+    } else if (units === METRIC) {
+      if (nominalCount < 0.001) {
+        suffix = 'μm';
+        pointResolution *= 1000000;
+      } else if (nominalCount < 1) {
+        suffix = 'mm';
+        pointResolution *= 1000;
+      } else if (nominalCount < 1000) {
+        suffix = 'm';
+      } else {
+        suffix = 'km';
+        pointResolution /= 1000;
+      }
+    } else if (units === US) {
+      if (nominalCount < 0.9144) {
+        suffix = 'in';
+        pointResolution *= 39.37;
+      } else if (nominalCount < 1609.344) {
+        suffix = 'ft';
+        pointResolution /= 0.30480061;
+      } else {
+        suffix = 'mi';
+        pointResolution /= 1609.3472;
+      }
+    }
+    return {
+      suffix,
+      pointResolution,
+    };
+  }
   render() {
     if (this.props.mapinfo && this.props.mapinfo.resolution !== null) {
-      const center = Proj.fromLonLat(this.props.map.center);
-      const units = this.props.units;
-      const pointResolutionUnits = units === DEGREES ? DEGREES : 'm';
-      const projection = Proj.get(this.props.mapinfo.projection);
-      const resolution = this.props.mapinfo.resolution;
-      let pointResolution = Proj.getPointResolution(projection, resolution, center, pointResolutionUnits);
-      if (units !== DEGREES) {
-        pointResolution *= projection.getMetersPerUnit();
-      }
-      let nominalCount = this.props.minWidth * pointResolution;
-      let suffix = '';
-      if (units === DEGREES) {
-        const metersPerDegree = Proj.METERS_PER_UNIT[DEGREES];
-        if (projection.getUnits() === DEGREES) {
-          nominalCount *= metersPerDegree;
-        } else {
-          pointResolution /= metersPerDegree;
-        }
-        if (nominalCount < metersPerDegree / 60) {
-          suffix = '\u2033'; // seconds
-          pointResolution *= 3600;
-        } else if (nominalCount < metersPerDegree) {
-          suffix = '\u2032'; // minutes
-          pointResolution *= 60;
-        } else {
-          suffix = '\u00b0'; // degrees
-        }
-      } else if (units === IMPERIAL) {
-        if (nominalCount < 0.9144) {
-          suffix = 'in';
-          pointResolution /= 0.0254;
-        } else if (nominalCount < 1609.344) {
-          suffix = 'ft';
-          pointResolution /= 0.3048;
-        } else {
-          suffix = 'mi';
-          pointResolution /= 1609.344;
-        }
-      } else if (units === NAUTICAL) {
-        pointResolution /= 1852;
-        suffix = 'nm';
-      } else if (units === METRIC) {
-        if (nominalCount < 0.001) {
-          suffix = 'μm';
-          pointResolution *= 1000000;
-        } else if (nominalCount < 1) {
-          suffix = 'mm';
-          pointResolution *= 1000;
-        } else if (nominalCount < 1000) {
-          suffix = 'm';
-        } else {
-          suffix = 'km';
-          pointResolution /= 1000;
-        }
-      } else if (units === US) {
-        if (nominalCount < 0.9144) {
-          suffix = 'in';
-          pointResolution *= 39.37;
-        } else if (nominalCount < 1609.344) {
-          suffix = 'ft';
-          pointResolution /= 0.30480061;
-        } else {
-          suffix = 'mi';
-          pointResolution /= 1609.3472;
-        }
-      }
+      const properties = this.calculateProperties();
+      const pointResolution = properties.pointResolution;
+      const suffix = properties.suffix;
       let i = 3 * Math.floor(
         Math.log(this.props.minWidth * pointResolution) / Math.log(10));
       let count, width;
